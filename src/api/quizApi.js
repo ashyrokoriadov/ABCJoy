8fc8d601dccced0988152c91652ad6cdb6c5a95f
Andriy Shyrokoryadov <andrey.sh@wp.pl>
2020-06-13 08:38:19 +0200

    Changed parameter for testing purposes

--- src/api/quizApi.js
+++ src/api/quizApi.js
@@ -2,7 +2,7 @@ const baseUrl = process.env.API_URL + "/";
 import { handleResponse, handleError } from "./apiUtils";
 
 export function getQuiz(abc, type) {
-  return fetch(baseUrl + abc + "?type=" + type + "&number=2")
+  return fetch(baseUrl + abc + "?type=" + type + "&number=3")
     .then(handleResponse)
     .catch(handleError);
 }
