7387ea9571b04ea2eb486ea7911bc20c3664687b
Andriy Shyrokoryadov <andrey.sh@wp.pl>
2020-07-19 21:52:32 +0200

    Implemendete store config based on env variable

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,19 @@ import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./components/App";
-import configureStore from "./store/configureStore.prod";
+import configureStoreProd from "./store/configureStore.prod";
+import configureStoreDev from "./store/configureStore.dev";
 import { Provider as ReduxProvider } from "react-redux";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./styles/style.css";
 
-const store = configureStore();
+let store;
+
+if (process.env.NODE_ENV === "production") {
+  store = configureStoreProd();
+} else {
+  store = configureStoreDev();
+}
 
 render(
   <ReduxProvider store={store}>
